078087472c21ef904730293b01f9c9c05e2cca0f
luca <lucasar.com8@gmail.com>
2023-10-14 17:05:11 +0200

    add panic example

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ use hal::{clock::*, flash::*, gpio::*, pcm::*, timer::*, watchdog::*};
 
 
 
-
 #[entry]
 // main
 fn main() -> ! {
@@ -21,45 +20,21 @@ fn main() -> ! {
     // Take the Peripherals
     let p = pac::Peripherals::take().unwrap();
 
-    // Watchdog Config.
-    let mut _watchdog = p.WDT_A.constrain();                                 // Setup WatchdogTimer
+    // disable wd timer to avoid reser
+    let wd = p.WDT_A.constrain();
+    let _ = wd.disable().unwrap();
 
-    _watchdog.set_timer_interval(TimerInterval::At31);
-    _watchdog.feed().unwrap();
-
-    // PCM Config.
-    let pcm = p.PCM.constrain()                                              // Setup PCM
-        .set_vcore(VCoreSel::DcdcVcore1)                                     // Set DCDC Vcore1 -> 48 MHz Clock
-        .freeze();
-    let _pcm_sel = pcm.get_powermode();                                      // Get the current powermode
-
-    // Flash Control Config.
-    let _flash_control = p.FLCTL.constrain()                                         // Setup Flash
-        .set_waitstates(FlashWaitStates::_2)                               // Two wait states -> 48 Mhz Clock
-        .freeze();
-
-    let _clock = p.CS.constrain()                                            // Setup CS
-        .mclk_dcosource_selection(DCOFrequency::_48MHz, MPrescaler::DIVM_0)  // 48 MHz DCO
-        .smclk_prescaler(SMPrescaler::DIVS_1)                                // 24 MHz SMCLK
-        .freeze();
-
-    hprintln!("Hello World Example - PCM: {}", _pcm_sel as u32);
+    hprintln!("A simple blink example");
 
     let gpio = p.DIO.split();
-    let mut red = gpio.p2_1.into_output();
-
-    let mut tim0 = p.TIMER_A0.constrain().set_clock(_clock);
-    let count = Count(10, TimerUnit::Hertz);
-    tim0.start(count).unwrap();
+    let mut red = gpio.p2_0.into_output();
 
 
-    red.set_high().unwrap();
-
-    loop {
-        _watchdog.feed().unwrap();
+    for _ in 0..10 {
         red.toggle().unwrap();
-        block!(tim0.wait()).unwrap();
+        hprintln!("A simple blink example");
     }
+    panic!("my_panic");
 }
 
 
@@ -69,6 +44,7 @@ use core::sync::atomic::{self, Ordering};
 #[inline(never)]
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    hprintln!("PANIC: {:?}",_info);
     loop {
         atomic::compiler_fence(Ordering::SeqCst);
     }
